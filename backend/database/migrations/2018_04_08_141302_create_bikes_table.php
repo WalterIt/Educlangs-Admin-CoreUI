f9f3fc8bb9a81446bdef1f22b979f72deb0e6ffc
Valto Silva <valtosilva7@gmail.com>
2018-12-20 12:59:46 -0500

    SUCCESSFUL Refactoring_Motorbikes_App_in_BACKEND_5.7

--- backend/database/migrations/2018_04_08_141302_create_bikes_table.php
+++ backend/database/migrations/2018_04_08_141302_create_bikes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bikes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('make');
+            $table->string('model');
+            $table->string('year');
+            $table->text('mods');
+            $table->string('picture');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('builder_id');
             $table->timestamps();
         });
     }
@@ -30,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bikes');
     }
 }
--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -5,12 +5,18 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(BikesTableSeeder::class);
+        $this->call(BuildersTableSeeder::class);
+        $this->call(ItemsTableSeeder::class);
+        $this->call(GaragesTableSeeder::class);
+        $this->call(BikeGarageTableSeeder::class);
+        $this->call(RatingsTableSeeder::class);
     }
 }
